82b3688e7797e54f3414456dac20c65cb7e4d0aa
brentmc <brentmcivor.mail+workgithub@gmail.com>
2017-01-19 18:03:15 +1000

    First Commit

---
+++ modules/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { IndexLink } from 'react-router'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+        	<li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+					<li><NavLink to="/repos" activeClassName="active">Repos</NavLink></li>
+        </ul>
+
+        {/* add this */}
+        {this.props.children}
+
+      </div>
+    )
+  }
+})
